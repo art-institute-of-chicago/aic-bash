47991d4953717bdc3071b663d0f89121ba1fad52
Illya Moskvin <ivmoskvin@gmail.com>
2018-12-23 03:10:26 -0600

    Add --fill flag for background colors
    Balancing performance vs. code readability. Performance wins here.

--- aic.sh
+++ aic.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# Check what flags were passed
+while test $# != 0
+do
+    case "$1" in
+    -f|--fill) OPT_FILL=--fill ;;
+    *)  usage ;;
+    esac
+    shift
+done
+
 IMAGE_ID="d02e0079-8e82-733e-683c-cb83a387ee5e"
 IMAGE_PATH="/tmp/default.jpg"
 
 curl -s "https://www.artic.edu/iiif/2/$IMAGE_ID/full/80,/0/default.jpg" --output "$IMAGE_PATH"
 
-INPUT="$(jp2a --term-fit --color --html "$IMAGE_PATH")"
+INPUT="$(jp2a --term-fit --color --html $OPT_FILL "$IMAGE_PATH")"
 
 # Remove HTML tags from beginning and end
 INPUT="${INPUT:449}"
@@ -22,41 +32,92 @@ INPUT="${INPUT//<br\/>/$'\n'}"
 # Start building our output for rendering
 OUTPUT=''
 
-# Split HTML by <br/> into rows
-while IFS=$'\n' read -ra ROWS; do
-    for ROW in "${ROWS[@]}"; do
 
-        # Transform spans into space-separated quadruples of R G B [Char], using pipes as span-separators
-        ROW="$(echo "$ROW" | sed -re "s/<span style='color:#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2});'>(.)<\/span>/\1 \2 \3 \4|/g")"
+if [ -z "$OPT_FILL" ]; then
 
-        # Discard the last pipe
-        ROW="${ROW::-1}"
+    # Split HTML by <br/> into rows
+    while IFS=$'\n' read -ra ROWS; do
+        for ROW in "${ROWS[@]}"; do
 
-        # Split row into columns using pipes
-        while IFS='|' read -ra COLS; do
-            for COL in "${COLS[@]}"; do
+            # Transform spans into space-separated quadruples of R G B [Char], using pipes as span-separators
+            ROW="$(echo "$ROW" | sed -re "s/<span style='color:#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2});'>(.)<\/span>/\1 \2 \3 \4|/g")"
 
-                # Split column into values using spaces
-                COL=($COL)
+            # Discard the last pipe
+            ROW="${ROW::-1}"
 
-                # Convert RGB hex to decimal
-                R="$(( 16#${COL[0]} ))"
-                G="$(( 16#${COL[1]} ))"
-                B="$(( 16#${COL[2]} ))"
+            # Split row into columns using pipes
+            while IFS='|' read -ra COLS; do
+                for COL in "${COLS[@]}"; do
 
-                # Get character and fix spaces
-                C="${COL[3]//&/ }"
+                    # Split column into values using spaces
+                    COL=($COL)
 
-                #  https://gist.github.com/XVilka/8346728
-                OUTPUT+="\033[38;2;${R};${G};${B}m${C}\033[0m"
+                    # Convert RGB hex to decimal
+                    R="$(( 16#${COL[0]} ))"
+                    G="$(( 16#${COL[1]} ))"
+                    B="$(( 16#${COL[2]} ))"
 
-            done
-        done <<< "$ROW"
+                    # Get character and fix spaces
+                    C="${COL[3]//&/ }"
 
-        OUTPUT+='\n'
+                    #  https://gist.github.com/XVilka/8346728
+                    OUTPUT+="\033[38;2;${R};${G};${B}m${C}\033[0m"
 
-    done
-done <<< "$INPUT"
+                done
+            done <<< "$ROW"
+
+            OUTPUT+='\n'
+
+        done
+    done <<< "$INPUT"
+
+else
+
+    # Split HTML by <br/> into rows
+    while IFS=$'\n' read -ra ROWS; do
+        for ROW in "${ROWS[@]}"; do
+
+            # Transform spans into space-separated quadruples of R G B [Char], using pipes as span-separators
+            ROW="$(echo "$ROW" | sed -re "s/<span style='color:#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2}); background-color:#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2});'>(.)<\/span>/\1 \2 \3 \4 \5 \6 \7|/g")"
+
+            # Discard the last pipe
+            ROW="${ROW::-1}"
+
+            # Split row into columns using pipes
+            while IFS='|' read -ra COLS; do
+                for COL in "${COLS[@]}"; do
+
+                    # Split column into values using spaces
+                    COL=($COL)
+
+                    # Handle the background color
+                    R="$(( 16#${COL[3]} ))"
+                    G="$(( 16#${COL[4]} ))"
+                    B="$(( 16#${COL[5]} ))"
+
+                    # https://gist.github.com/XVilka/8346728
+                    OUTPUT+="\033[48;2;${R};${G};${B}m"
+
+                    # Handle the foreground color
+                    R="$(( 16#${COL[0]} ))"
+                    G="$(( 16#${COL[1]} ))"
+                    B="$(( 16#${COL[2]} ))"
+
+                    # Get character and fix spaces
+                    C="${COL[6]//&/ }"
+
+                    # https://gist.github.com/XVilka/8346728
+                    OUTPUT+="\033[38;2;${R};${G};${B}m${C}\033[0m"
+
+                done
+            done <<< "$ROW"
+
+            OUTPUT+='\n'
+
+        done
+    done <<< "$INPUT"
+
+fi
 
 printf "$OUTPUT"
 
