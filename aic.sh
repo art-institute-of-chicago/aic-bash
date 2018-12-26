5939d47cca0ad8f3fe151d6d39fe28384907750a
Illya Moskvin <imoskvin@artic.edu>
2018-12-26 12:36:44 -0600

    Avoid negative offsets in substring expressions

--- aic.sh
+++ aic.sh
@@ -367,9 +367,10 @@ INPUT="$(jp2a --term-fit --color --html $OPT_FILL "$FILE_IMAGE")"
 export LINES="$OLD_LINES"
 
 # Remove HTML tags from beginning and end
+# https://superuser.com/questions/1033273/bash-4-3-substring-negative-length-on-os-x
 INPUT="${INPUT:449}"
-INPUT="${INPUT::-29}"
-INPUT="${INPUT::-5}" # <br/>
+INPUT="${INPUT::${#INPUT}-29}"
+INPUT="${INPUT::${#INPUT}-5}" # <br/>
 
 # Replace &nbsp; with a placeholder character that's not in the character map
 # https://github.com/cslarsen/jp2a/blob/61d205f6959d88e0cc8d8879fe7d66eb0932ecca/src/options.c#L69
@@ -393,7 +394,7 @@ if [ ! "$OPT_FILL" = '--fill' ]; then
             ROW="$(echo "$ROW" | sed -E "s/<span style='color:#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2});'>(.)<\/span>/\1 \2 \3 \4|/g")"
 
             # Discard the last pipe
-            ROW="${ROW::-1}"
+            ROW="${ROW::${#ROW}-1}"
 
             # Split row into columns using pipes
             while IFS='|' read -ra COLS; do
@@ -432,7 +433,7 @@ else
             ROW="$(echo "$ROW" | sed -E "s/<span style='color:#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2}); background-color:#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2});'>(.)<\/span>/\1 \2 \3 \4 \5 \6 \7|/g")"
 
             # Discard the last pipe
-            ROW="${ROW::-1}"
+            ROW="${ROW::${#ROW}-1}"
 
             # Split row into columns using pipes
             while IFS='|' read -ra COLS; do
